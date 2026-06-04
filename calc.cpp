b7876fba5475121ac64c8b787618b97d4694ca13
aubergine_ux <126033183+aubergine-ux@users.noreply.github.com>
2026-06-04 07:30:58 -0400

    CC 26.1

---
+++ calc.cpp
@@ -0,0 +1,32 @@
+# include <iostream>
+# include <cmath>
+
+
+using namespace std;
+
+int main() {
+
+    string operation;
+    double number1, number2;
+
+    cout << "Enter First Number: ";
+    cin >> number1;
+    cout << "Enter Operation (+, -, *, /): ";
+    cin >> operation;
+    cout << "Enter Second Number: ";
+    cin >> number2;
+
+    if (operation == "+") {
+        cout << number1 + number2;
+    } else if (operation == "-") {
+        cout << number1 - number2;
+    } else if (operation == "*") {
+        cout << number1 * number2;
+    } else if (operation == "/") {
+        cout << number1 / number2;
+    } else {
+        cout << "Not Recognized as an Operation!";
+    }
+
+    return 0;
+}
